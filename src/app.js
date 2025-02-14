70537b7c789479cb675c42a4323d9b22b6b2d233
Vishal Nema <vishalnema23@gmail.com>
2025-02-14 16:24:50 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -4,20 +4,12 @@ const app = express();
 
 
 // (get) this will only handle to GET call /user
-app.get("/user",(req, res) => {
+app.get("/user/:userId/:name/:password",(req, res) => {
+    console.log(req.params);
     res.send({firstname: "Vishal", lastname: "Nema "})
 })
-app.post("/user",(req, res) => {
-    res.send("data successfully saved to the database")
-})
-app.delete("/user",(req, res) => {
-    res.send("delete data successfully")
-})
+//user/707/vishal/xyz
 
-// (use) this will match all the http methods
-app.use("/test",(req , res) =>{
-    res.send("hello this is test")
-})
 app.listen(7777,()=>{
     console.log('Server is running on port 7777');
 })
