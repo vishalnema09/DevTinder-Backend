dd6b3345135bf0b51cde33c072b48d99a6af9ddb
Vishal Nema <vishalnema23@gmail.com>
2025-02-15 11:35:17 +0530

    multiple rountes handlers

--- src/app.js
+++ src/app.js
@@ -1,15 +1,39 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
+// app.use("/route", rH , [rH2 , rH3] , rH4 , rH5);
 
-// (get) this will only handle to GET call /user
-app.get("/user/:userId/:name/:password",(req, res) => {
-    console.log(req.params);
-    res.send({firstname: "Vishal", lastname: "Nema "})
-})
+
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("handling the first route user");
+    next();
+  },
+  (req, res, next) => {
+    console.log("handling the second route user");
+    // res.send("2nd response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("handling the 3 route user");
+    // res.send("3rd response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("handling the 4 route user");
+    // res.send("4th response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("handling the 5 route user");
+    // res.send("5th response");
+    next();
+  },
+);
 //user/707/vishal/xyz
 
-app.listen(7777,()=>{
-    console.log('Server is running on port 7777');
-})
+app.listen(7777, () => {
+  console.log("Server is running on port 7777");
+});
