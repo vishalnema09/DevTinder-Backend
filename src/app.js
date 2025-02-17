4cb9a43bd66b95fafdaae0e8114aaec3a8acf2a8
Vishal Nema <vishalnema23@gmail.com>
2025-02-17 13:12:00 +0530

    dynamic data  in  signup page

--- src/app.js
+++ src/app.js
@@ -1,18 +1,15 @@
 require('dotenv').config()
 const express = require("express");
-const connectDB = require(".config/database");
+const connectDB = require("./config/database");
 const app = express();
 const User  = require("./models/user");
 const {adminAuth, userAuth}= require("./middlewares/auth")
 
-
+app.use(express.json());
 app.post("/signup", async (req , res)=>{
-    const user = new User({
-        firstName: "harsh",
-        lastName: "pathak",
-        email: "harsh@gmail.com",
-        password: "123456"
-    });
+    //   console.log(req.body)
+
+    const user = new User(req.body);
 
     try{
         await user.save();
--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
-const connectDb = async () => {
+const connectDB = async () => {
     await mongoose.connect(process.env.PORT)
 }
 
-module.exports = connectDb;
+module.exports = connectDB;
