2fd52df2851932e049b54f0ed841045d60cc7c08
Vishal Nema <vishalnema23@gmail.com>
2025-02-18 15:37:29 +0530

    auth

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const { validateSignUpData } = require("./utils/validation");
 const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
+const {auth} = require("./middlewares/auth");
 
 app.use(express.json());
 app.use(cookieParser());
@@ -63,28 +64,11 @@ app.post("/login", async (req, res) => {
   }
 });
 
-app.get("/profile", async (req, res) => {
+app.get("/profile" , userAuth , async (req, res) => {
   try {
-    const cookies = req.cookies;
 
-    const { token } = cookies;
+    const user = req.user;
 
-    if(!token){
-      throw new Error("No token, authorization denied");
-    }
-
-    //validate my token
-    const decodedMessage = await jwt.verify(token, "DEV@TINDER$790");
-    // console.log(decodedMessage);
-    const { _id } = decodedMessage;
-
-    const user = await User.findById(_id);
-
-    if(!user){
-      throw new Error("User not found");
-    }
-
-    // console.log(cookies);
     res.send(user);
   } catch (err) {
     res.status(403).send("Unauthorized access");
@@ -120,7 +104,7 @@ app.get("/feed", async (req, res) => {
 });
 
 //user user
-app.delete("/user", async (req, res) => {
+app.delete("/user",async (req, res) => {
   const userID = req.body.userID;
   try {
     const user = await User.findByIdAndDelete(userID);
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,30 +1,28 @@
-const adminAuth =(req,res,next)=>{
-    // logic
-    const token = "xyz";
-    const isAdminAuthorized = token === "xyz";
-    if(!isAdminAuthorized){
-        res.status(403).send("You do not have permission to");
-    }
-    else {
-        next();
-    }
-}
-const userAuth =(req,res,next)=>{
-    // logic
-    const token = "xyz";
-    const isAdminAuthorized = token === "xyz";
-    if(!isAdminAuthorized){
-        res.status(403).send("Unauthorized access");
-    }
-    else {
-        next();
-    }
-}
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
+const userAuth = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      throw new Error("not authenticated");
+    }
 
+    const decodedOjb = await jwt.verify(token, "DEV@TINDER$790");
+    const { _id } = decodedOjb;
 
+    const user = await User.findById(_id);
+
+    if (!user) {
+      throw new Error("user not find");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(404).send("Error: " + error.message);
+  }
+};
 module.exports = {
-    adminAuth,
-    userAuth,
-
+  userAuth,
 };
